f0eb60c64cb38792ad094fd7f499934ddf3be4a3
kitkat119 <gprydal@yahoo.co.uk>
2017-07-19 15:46:55 +0100

    enabled sessions

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_one_name = params[:player_one_name]
-    @player_two_name = params[:player_two_name]
+    session[:player_one_name] = params[:player_one_name]
+    session[:player_two_name] = params[:player_two_name]
+    redirect to('/play')
+  end
+
+  get '/play' do
+    @player_one_name = session[:player_one_name]
+    @player_two_name = session[:player_two_name]
     erb :play
   end
 
