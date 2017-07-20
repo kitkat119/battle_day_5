48b0bc723344983342ef94a4f73c5a70c0efb2ac
chuk-chuk <yulia.v.litvin@gmail.com>
2017-07-20 11:38:06 +0100

    add attack feature test and new attck.erb

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,6 +1,6 @@
 feature 'viewing hit points' do
   scenario "Player one can view player 2's hit points" do
-  sign_in_and_play
+    sign_in_and_play
     expect(page).to have_content 'Kat: 60HP'
   end
 end
