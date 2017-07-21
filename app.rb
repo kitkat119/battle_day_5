3df7d1294dcc1ec3554b58cf6c5ac6ec2b10089c
kitkat119 <gprydal@yahoo.co.uk>
2017-07-21 10:48:37 +0100

    controller made skinnier

--- app.rb
+++ app.rb
@@ -10,22 +10,20 @@ enable :sessions
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $player_1.name #yulia
-    @player_2_name = $player_2.name #kat
-    @player_2_points = $player_2.points #60
+    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    Game.new.attack(@player_2)
+    @game = $game
+    @game.attack(@game.player_2)
     erb :attack
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,17 @@
 require 'game'
 
 describe Game do
-  subject(:game) {described_class.new}
+  subject(:game) {described_class.new(player_1, player_2)}
   let(:player_1) { double :player }
   let(:player_2) { double :player }
+
+it 'has a first player' do
+  expect(game.player_1).to eq player_1
+end
+
+it 'has a second player' do
+  expect(game.player_2).to eq player_2
+end
 
   context "attack" do
     it "damages the player" do
